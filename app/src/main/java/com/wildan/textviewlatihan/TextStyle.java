87c6a60a4daa6383b7b18caddf37cd38304512c7
pmlprogamer <pmlprogamer@gmail.com>
2019-09-24 11:28:41 +0700

    Initial commit

---
+++ app/src/main/java/com/wildan/textviewlatihan/TextStyle.java
@@ -0,0 +1,14 @@
+package com.wildan.textviewlatihan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextStyle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_style);
+    }
+}
